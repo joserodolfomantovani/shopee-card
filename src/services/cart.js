a724151f40cd0d116c588220b4294226163c2eb4
Rodolfo <j10rodolfo@gmail.com>
2025-08-28 21:20:28 -0300

    Adicionando as Assinaturas do Módulo Cart e Item

---
+++ src/services/cart.js
@@ -0,0 +1,16 @@
+async function addItem(useCart){
+
+}
+
+async function deleteItem(useCart, name){
+
+}
+
+async function removeItem(useCart, index) {
+    
+}
+
+async function calculateTotal(useCart) {
+    
+    
+}
