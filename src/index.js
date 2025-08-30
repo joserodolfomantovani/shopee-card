a651b425aa6a96db69e4b7f87e148d3b203ca3b4
Rodolfo <j10rodolfo@gmail.com>
2025-08-30 10:18:29 -0300

    Implmentando a lógica do item

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-const car = []
+import createItem from "./services/item.js"
+
+const cart = []
+
+const item1 = await createItem("hotwheels ferrari", 20.99, 1)
+const item2 =  await createItem("hotwheels lamborghini", 30.99, 3)
+
+console.log(item2.subtotal());
